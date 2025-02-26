2a345c1d58cb53eda165bd3032bd77222da9975d
GABRIEL_ALEJANDRO_MONTES_HERNANDEZ <a.22308051281276@cbtis128.edu.mx>
2025-02-26 20:09:04 +0000

    practica icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyIcon());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyIcon extends StatelessWidget {
+  const MyIcon({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Gabriel Montes ',
+          ), //Mat:22308051281276 Gpo 6J
+          titleTextStyle: const TextStyle(fontSize: 30, color: Colors.black),
+          centerTitle: true,
+          backgroundColor: Colors.black26,
+        ),
+        body: const Column(
+          children: <Widget>[
+            Text(
+              'Gabriel Montes Mat:22308051281276 Gpo 6J',
+              style: (TextStyle(fontSize: 17, color: Colors.black)),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.music_off_rounded,
+                  color: Colors.red,
+                  size: 30.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                Icon(
+                  Icons.audiotrack,
+                  color: Colors.purpleAccent,
+                  size: 30.0,
+                ),
+                Icon(
+                  Icons.music_note_outlined,
+                  color: Colors.blue,
+                  size: 30.0,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
